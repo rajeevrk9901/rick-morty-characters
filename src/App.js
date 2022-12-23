c750ba016482eb016b158e585c50c61f4d9dafe6
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-12-23 15:03:09 +0530

    styling done for medium screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route } from 'react-router-dom';
+// import Home from './component/Home';
 import CharacterDetail from './component/CharacterDetail';
 import CharacterList from './component/CharacterList';
 import './App.scss';
 import React from 'react';
 
+
 function App() {
   return (
     <Routes>
-      <Route path="/characters" exact element={<CharacterList />} />
+      {/* <Route exact path="/" element={<Home />} /> */}
+
+      {/* <Route exact path="/" element={CharacterList} >
+        redirect("/characters")
+      </Route> */}
+      <Route exact path="/" element={<Navigate to="/characters" />} />
+      <Route exact path="/characters" element={<CharacterList />} />
       <Route path="/characters/:id" element={<CharacterDetail />} />
     </Routes>
-
 
   );
 }
